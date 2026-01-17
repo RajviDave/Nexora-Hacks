b603e2dd765d96ffee8ab4da81b5ba5555fe959b
RajviDave <rajvidave22@gmail.com>
2026-01-17 21:39:58 +0530

    login done

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
     <h1 class="heading">SKILL SYNC</h1>
     <div class="main-div">
         <form class="form" action=<?php echo $_SERVER['PHP_SELF'];?> method="post" >
-            <label><b>Username</b></label><br>
-            <input type="text" value="Username" name="username"><br><br><br>
+            <label><b>EmailID</b></label><br>
+            <input type="text" value="xyz@gmail.com" name="email"><br><br><br>
             <label><b>Password</b></label><br>
             <input type="password" value="qwertyui" name="passwd"><br><br><br>
             <input type="submit" name="submit" value="SignIn"><br><br>
@@ -25,5 +25,32 @@
 </html>
 
 <?php
-    
+include("db.php");
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $email = $_POST["email"];
+    $password = $_POST["passwd"];
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    if($result->num_rows == 1){
+
+        $user = $result->fetch_assoc();
+
+        if(password_verify($password, $user["password"])){
+            echo "Login successful ✅";
+            // start session / redirect
+        } else {
+            echo "Wrong password ❌";
+        }
+
+    } else {
+        echo "User not found ❌";
+    }
+}
 ?>
